826527c2450636c21e74bc5216df840fa41eba21
Sharky <coolgaming2016@outlook.com>
2018-09-09 10:46:48 -0400

    .

---
+++ math_ai.py
@@ -0,0 +1,12 @@
+import math
+firstnumber = 1+1
+print(1+1)
+firstnumber = firstnumber + 1
+print(firstnumber)
+firstnumber = firstnumber + 1
+print(firstnumber)
+#Loop
+while (firstnumber > 2):
+    firstnumber = firstnumber + 1
+    print(firstnumber)
+#I know this isnt really an AI... i tried.
